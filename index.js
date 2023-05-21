eb5067652c76fe179ba32ffa7b8c32baed460134
Voidberry <98706885+V0idberry@users.noreply.github.com>
2023-05-21 23:52:45 +0200

    Created an event listener for range input. Created a function that clears previous grid.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const grid = document.querySelector(".grid");
+const slider = document.getElementById("range");
+const displayValue = document.getElementById("value");
+
+displayValue.innerHTML = 'Please select the desired grid size.'
+
+slider.addEventListener('input', function() {
+    clearGrid();
+    createGrid(slider.value);
+    displayValue.innerHTML = `Current grid size: ${slider.value} x ${slider.value}`;
+})
 
 function createGrid(gridSize) {
     for (i = 0; i < gridSize * gridSize; i++) {
         const newBox = document.createElement("div");
-        newBox.classList.add("box");
         grid.appendChild(newBox);
+        newBox.classList.add("box");
         newBox.style.height = `calc(100% / ${gridSize})`;
         newBox.style.width = `calc(100% / ${gridSize})`;
     }
 }
 
-createGrid(10);
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.firstChild.remove();
+    }
+}
